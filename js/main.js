ec5cebc6a0d54a0cf0c0b34b4315575b8be8320f
ALi <s.arme@ya.ru>
2020-04-04 11:02:45 +0430

    Store instances of event class and return them instead of creating same event

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
-
-class Event{
+class Event {
   state = false;
   name = "";
-  constructor(state, func, name) {
-    this.state = state;
-    this.name = name;
+
+  constructor(state , func, name) {
+    switch (arguments.length) {
+      case 0:
+        //do basic code
+        break;
+      case 1:
+        for (let i = 0; i < Event.instances.length; i++) {
+          if (Event.instances[i].name === name) {
+            return name;
+          }
+        }
+        this.state = true;
+        this.name = name;
+        Class.instances.push(this);
+        break;
+      case 3:
+        this.state = state;
+        this.name = name;
+        Class.instances.push(this);
+      default:
+        //do code with `a` & `b`
+        break;
+    }
   }
 }
 
@@ -14,13 +34,10 @@ class Eventor {
     this.listerners = []
   }
 
-  addListerner(...args){
+  addListerner(...args) {
     const { listerners } = this;
     for (var i = 0; i < arguments.length; i++) {
-    listerners.concat(new Listerner(
-      false,
-      typeof arguments[i] == "function" ? arguments[i] : new Function(arguments[i]),
-      `${arguments[i]}`))
-  }
+      listerners.concat(new Listerner(arguments[i]))
+    }
   }
 }
